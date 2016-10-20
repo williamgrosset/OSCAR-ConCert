d153f3a1c6dadc6d0abb9828d13c9f3d0bfe2871
William Grosset <will_grosset@hotmail.com>
2016-10-20 05:32:12 +0000

    searchDirectory, grabFiles, verifyFiles methods (multiple deployments)

--- java/Audit.java
+++ java/Audit.java
@@ -98,40 +98,15 @@ public class Audit {
     private static void verifyOscar() {
         File directory = searchForDirectory("/var/lib/tomcat7", ".*catalina.base.*");
         Stack<String> files = grabFiles(directory, "^(oscar)[0-9]*?.*(properties)$");
+        
+        // Verify files
         /*
         * while (!files.empty())
         *   files.pop()
-        *   oscarBuild(file)
-        *   verifyOscarProperties(file)
+        *   oscarBuild(file.pop()) // Keep file as String or as File object?
+        *   verifyOscarProperties(file.pop())
         */
     }
-
-    /*
-    * TODO: 
-    * - Check to see where this file is located (and what it could be named)
-    */
-	private static void oscarBuild() {
-		String s;
-		File oscar = new File ("/usr/share/tomcat7/oscar.properties");
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(new ReverseLineInputStream(oscar)));
-			boolean isMatch = false;
-			while ((s = br.readLine()) != null) {
-            if (Pattern.matches("^(#).*", s))
-                continue;
-            isMatch = Pattern.matches("^(buildtag=).*", s);
-            if (isMatch) {
-                System.out.println("Build and version: " + s.substring(9));
-                break;
-            }
-			}
-			if (!isMatch) {
-				System.out.println("Oscar build/version cannot be found.");
-			}
-		} catch (Exception e) {
-			System.out.println("Oscar build error: " + e);
-		}
-	}	
 
 	/*
     *  - Search in $CATALINA_BASE/webapps directory for all folders that could be deployed
@@ -141,8 +116,8 @@ public class Audit {
     *  - Return directory name
 	*  - Fix RE for in pattern
 	*/
-	private static File searchForDirectory(String defaultPath, String pattern) {
-		String pathName = "";
+	private static File searchForDirectory(String defaultPath, String regex) {
+		CharSequence pathName = "";
 		Stack<String> files = new Stack<String>();
 		boolean isMatch = false;
 		File directory = new File(defaultPath);
@@ -153,25 +128,30 @@ public class Audit {
             Process p = Runtime.getRuntime().exec(new String[]{"sh", "-c", "ps -ef | grep tomcat7"});
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             while ((s = br.readLine()) != null) {
-                isMatch = Pattern.matches(pattern, s); // may not need this -> use RE to grab starting point of substring
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(s);
+                isMatch = matcher.matches();
+                //isMatch = Pattern.matches(regex, s);
 				System.out.println("Did we get a match?: " + isMatch);
                 if (isMatch) {
-					pathName = s.substring(0,11); // take substring (FIX)
-                    System.out.println("Currently in TRY/CATCH block: " + pathName); // check if path is correct
+                    // Properly take substring (start from match.end() up until we hit whitespace -> pathName)
+                    pathName = matcher.group(0);
+					//pathName = cs.subSequence(60, 80);
+                    System.out.println("Currently in TRY/CATCH block, our value of pathname: " + pathName); // check if path is correct
 					break;
                 }
             }
-            System.out.println("VALUE OF S: " + s); // currently null
+            System.out.println("VALUE OF S (line we are reading through): " + s);
             p.destroy();
             if (!isMatch) {
                 System.out.println("DEFAULT PATH USED: " + directory);
-                return directory;
+                return new File(defaultPath);
             } else {
                 System.out.println("PATHNAME FOUND AND USED: " + directory);
-                return new File(pathName);
+                return new File(pathName.toString());
             }
         } catch (Exception e) {
-            System.out.println("Process check for Tomcat7 failed (also using defaultPath): " + e);
+            System.out.println("Process check for Tomcat7 failed (using defaultPath): " + e);
             return new File(defaultPath);
         }
 	}
@@ -180,7 +160,7 @@ public class Audit {
     *  - Loop through folders/files in directory
     *  - Push files onto Stack w/ pattern
     */
-    private static Stack<String> grabFiles(File directory, String pattern) {
+    private static Stack<String> grabFiles(File directory, String regex) {
         String[] fileList = directory.list();
         Stack<String> files = new Stack<String>();
         
@@ -194,7 +174,7 @@ public class Audit {
         // List all possible file(s) that we are looking for
         System.out.println("Adding all possible file(s):");
         for (int i = 0; i < fileList.length; i++) {
-                if (Pattern.matches(pattern, fileList[i])) {
+                if (Pattern.matches(regex, fileList[i])) {
                      System.out.println(fileList[i]);
                      files.push(fileList[i]);
                 }
@@ -203,7 +183,7 @@ public class Audit {
         // We did not find a file   
         if (files.empty()) {
             System.out.println("Our stack is empty -> return false.");
-            return null;
+            return files;
         }         
         return files;    
     }
@@ -212,9 +192,36 @@ public class Audit {
     * TODO: 
     * - Check to see where this file is located (and what it could be named)
     */
-	private static void verifyOscarProperties() {
+    private static void oscarBuild(String fileName) {
+        String s;
+        File oscar = new File (fileName);
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new ReverseLineInputStream(oscar)));
+            boolean isMatch = false; 
+            while ((s = br.readLine()) != null) {
+                if (Pattern.matches("^(#).*", s))
+                    continue;
+                isMatch = Pattern.matches("^(buildtag=).*", s);
+                if (isMatch) {
+                    System.out.println("Build and version: " + s.substring(9));
+                    break;
+                }   
+            }
+            if (!isMatch) {
+                System.out.println("Oscar build/version cannot be found.");
+            }   
+        } catch (Exception e) {
+            System.out.println("Oscar build error: " + e);
+        }   
+    }
+
+    /*
+    * TODO: 
+    * - Check to see where this file is located (and what it could be named)
+    */
+	private static void verifyOscarProperties(String fileName) {
 		String s;
-        File oscar = new File("/usr/share/tomcat7/oscar.properties");
+        File oscar = new File(fileName);
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(new ReverseLineInputStream(oscar)));
             boolean isMatch1 = false;
@@ -229,19 +236,19 @@ public class Audit {
 				isMatch1 = Pattern.matches("^(HL7TEXT_LABS=).*", s);
                 isMatch2 = Pattern.matches("^(SINGLE_PAGE_CHART=).*", s);
 				isMatch3 = Pattern.matches("^(TMP_DIR=).*", s);
-                if (isMatch1) {
+                if (isMatch1) { // HL7TEXT_LABS=
 					if (s.substring(13).toLowerCase().equals("yes")) {
 						flag1 = true;
 						System.out.println(s);
 					}
                 }
-				if (isMatch2) {
+				if (isMatch2) { // SINGLE_PAGE_CHART=
 					if (s.substring(18).toLowerCase().equals("true")) {
 						flag2 = true;
 						System.out.println(s);
 					}
 				}
-				if (isMatch3) {
+				if (isMatch3) { // TMP_DIR=
 					if (!s.substring(8).equals("")) {
 						flag3 = true;
 						System.out.println(s);
@@ -260,13 +267,15 @@ public class Audit {
             System.out.println("Oscar verification error: " + e);
         }
 	}
+    
+    private static void verifyDrugref() {}
 
     /*
     * TODO: 
     * - Check to see where this file is located (and what it could be named)
 	* - Remove hard code for port number (use guest port)
     */
-	private static void verifyDrugref() {
+	private static void verifyDrugrefProperties() {
         String s;
         File drugref = new File("/usr/share/tomcat7/drugref2.properties");
         try {
@@ -286,25 +295,25 @@ public class Audit {
                 isMatch2 = Pattern.matches("^(db_url=).*", s);
                 isMatch3 = Pattern.matches("^(db_driver=).*", s);
                 isMatch4 = Pattern.matches("^(drugref_url=).*", s);
-                if (isMatch1) {
+                if (isMatch1) { // db_user=
                     if (!s.substring(8).equals("")) {
                         flag1 = true;
                         System.out.println(s);
                     }
                 }
-                if (isMatch2) {
+                if (isMatch2) { // db_url=
                     if (s.substring(7).toLowerCase().equals("jdbc:mysql://127.0.0.1:3306/drugref")) { // will port value &
                         flag2 = true;        							                              // drugref always be
                         System.out.println(s);							                              // these values?
                     }
                 }
-                if (isMatch3) {
+                if (isMatch3) { // db_driver=
                     if (s.substring(10).toLowerCase().equals("com.mysql.jdbc.driver")) {
                         flag3 = true;
                         System.out.println(s);
                     }
                 }
-				if (isMatch4) {
+				if (isMatch4) { // drugref_url=
 					if (!s.substring(12).toLowerCase().equals("")) { 
 						flag4 = true;
 						System.out.println(s);
@@ -360,7 +369,8 @@ public class Audit {
 			//serverVersion();
 			//JVMTomcat7();
 			//mysqlVersion();
-			oscarBuild();
+            //verifyOscar();
+			//oscarBuild();
 			//verifyOscar();
 			//verifyDrugref();
 			//verifyTomcat();
