ae19852a43e4224b59c351a11952fb77be6f2fb0
William Grosset <will_grosset@hotmail.com>
2016-10-29 04:31:20 +0000

    added code review suggestions + error messages

--- java/Audit.java
+++ java/Audit.java
@@ -12,20 +12,30 @@ import java.util.regex.Pattern;
 */
 public class Audit {
     
-    /* 
+    /*
     *  serverVersion():
-    *  Run "lsb_release -r" command and extract release value.
+    *  Read "/etc/lsb-release" file and extract Ubuntu server version.
     */
     private static void serverVersion() {
         try {
-            String s;
-            Process p = Runtime.getRuntime().exec("lsb_release -r");
-            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while ((s = br.readLine()) != null)
-                System.out.println("Ubuntu server version: " + s.substring(9));
-            p.destroy();
+            String s = "";
+            File lsbRelease = new File("/etc/lsb-release");
+            BufferedReader br = new BufferedReader(new InputStreamReader(new ReverseLineInputStream(lsbRelease)));
+            boolean isMatch = false;
+
+            System.out.println("Currently checking Ubuntu server version...");
+            while ((s = br.readLine()) != null) {
+                isMatch = Pattern.matches("^(DISTRIB_DESCRIPTION=).*", s);
+                if (isMatch) {
+                    System.out.println("Ubuntu server version: " + s.substring(20));
+                    break;
+                }
+            }
+            if (!isMatch) {
+                System.out.println("Could not detect Ubuntu server version.");
+            }
         } catch (Exception e) {
-            System.out.println("Could not detect Ubuntu server version: " + e);
+            System.out.println("Could not run \"lsb release\" command to detect Ubuntu server version " + e);
         }
     }
 
@@ -44,7 +54,7 @@ public class Audit {
     */
     private static void JVMTomcat7(String binPath) {
         try {
-            String s;
+            String s = "";
             String cmd = new String(binPath + "/version.sh");
             Process p = Runtime.getRuntime().exec(cmd);
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -52,7 +62,8 @@ public class Audit {
             boolean isMatch2 = false;
             boolean flag1 = false;
             boolean flag2 = false;
-
+            
+            System.out.println("Currently checking JVM/Tomcat versions...");
             while ((s = br.readLine()) != null) {
                 isMatch1 = Pattern.matches("^(JVM Version:).*", s);
                 isMatch2 = Pattern.matches("^(Server version:).*", s);
@@ -73,7 +84,7 @@ public class Audit {
                 System.out.println("Tomcat version cannot be found.");
             p.destroy();
         } catch (Exception e) {
-            System.out.println("JVM/Tomcat7 bash script error: " + e);
+            System.out.println("Could not run \"version.sh\" bash script to detect JVM/Tomcat version(s): " + e);
         }
     }
 	
@@ -83,14 +94,16 @@ public class Audit {
     */
     private static void mysqlVersion() {
         try {
-            String s;
+            String s = "";
             Process p = Runtime.getRuntime().exec("mysql --version");
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            System.out.println("Currently checking MySQL version...");
             while ((s = br.readLine()) != null)
                 System.out.println("MySQL version: " + s.substring(11));
             p.destroy();
         } catch (Exception e) {
-            System.out.println("Could not detect MySQL version: " + e);
+            System.out.println("Could not run \"mysql --version\" command to detect MySQL version: " + e);
         }
     }
 
@@ -110,10 +123,43 @@ public class Audit {
         // Verify files on the Stack
         while (!files.empty()) {
             String file = files.pop();
+            // Verify "oscar_mcmaster.properties" file (not on Stack)
+            System.out.println("Currently checking \"" + file + "_mcmaster.properties\" file...");
+            oscarBuild("/var/lib/tomcat7/webapps/oscar/WEB-INF/classes/" + file + "_mcmaster"); // will this always be "oscar($)*_mcmaster.properties"?
+            // Verify properties file (on Stack)
             System.out.println("Currently checking \"" + file + ".properties\" file...");
             oscarBuild(catalinaHome+"/"+file);
             verifyOscarProperties(catalinaHome+"/"+file);
         }
+    }
+
+    /*
+    *  oscarMcmasterBuild(String: fileName):
+    *  Read Oscar buildtag of mcmaster properties file.
+    */ 
+    private static void oscarMcmasterBuild(String fileName) {
+        try {
+            String s = "";
+            File oscarMcmaster = new File(fileName + ".properties");
+            BufferedReader br = new BufferedReader(new InputStreamReader(new ReverseLineInputStream(oscarMcmaster)));
+            boolean isMatch = false;
+
+            while ((s = br.readLine()) != null) {
+                if (Pattern.matches("^(#).*", s))
+                    continue;
+                isMatch = Pattern.matches("^(buildtag=).*", s);
+                if (isMatch) {
+                    System.out.println("Oscar build and version: " + s.substring(9));
+                    break;
+                }
+            }
+            if (!isMatch) {
+                System.out.println("Oscar build/version cannot be found.");
+            }
+        } catch (Exception e) {
+            System.out.println("Could not read \"oscar_mcmaster.properties\" file to detect build: " + e);
+        }
+
     }
 
     /* 
@@ -122,7 +168,7 @@ public class Audit {
     */
     private static void oscarBuild(String fileName) {
         try {
-            String s;
+            String s = "";
             File oscar = new File(fileName + ".properties");
             BufferedReader br = new BufferedReader(new InputStreamReader(new ReverseLineInputStream(oscar)));
             boolean isMatch = false; 
@@ -140,7 +186,7 @@ public class Audit {
                 System.out.println("Oscar build/version cannot be found.");
             }   
         } catch (Exception e) {
-            System.out.println("Could not detect Oscar build and version: " + e);
+            System.out.println("Could not read properties file to detect Oscar build: " + e);
         }   
     }
 
@@ -151,7 +197,7 @@ public class Audit {
     */
     private static void verifyOscarProperties(String fileName) {
         try {
-            String s;
+            String s = "";
             File oscar = new File(fileName + ".properties");
             BufferedReader br = new BufferedReader(new InputStreamReader(new ReverseLineInputStream(oscar)));
             boolean isMatch1 = false;
@@ -168,7 +214,7 @@ public class Audit {
                 isMatch2 = Pattern.matches("^(SINGLE_PAGE_CHART=).*", s);
                 isMatch3 = Pattern.matches("^(TMP_DIR=).*", s);
                 if (isMatch1) { // HL7TEXT_LABS=
-                    if (s.substring(13).toLowerCase().equals("yes")) {
+                    if (s.substring(13).toLowerCase().equals("yes")) { // THIS needs to be fixed, what if there is a white space after '=' ??
                         flag1 = true;
                         System.out.println("\"HL7TEXT_LABS\" tag is configured properly.");
                     }
@@ -189,13 +235,13 @@ public class Audit {
                     break;
             }
             if (!flag1)
-                System.out.println("\"HL7TEXT_LABS\" tag not configured properly.");        
+                System.out.println("\"HL7TEXT_LABS\" tag is not set to \"yes\" and is not configured properly.");        
             if (!flag2)
-                System.out.println("\"SINGLE_PAGE_CHART\" tag not configured properly.");    
+                System.out.println("\"SINGLE_PAGE_CHART\" tag not set to \"true\" and is not configured properly.");    
             if (!flag3)
-                System.out.println("\"TMP_DIR\" tag not configured properly.");              
+                System.out.println("\"TMP_DIR\" tag is not set to a directory and is not configured properly.");              
         } catch (Exception e) {
-            System.out.println("Oscar verification error: " + e);
+            System.out.println("Could not read properties file to verify Oscar tags: " + e);
         }
     }
     
@@ -227,7 +273,7 @@ public class Audit {
     */
     private static void verifyDrugrefProperties(String fileName) {
         try {
-            String s;
+            String s = "";
             File drugref = new File(fileName + ".properties");
             BufferedReader br = new BufferedReader(new InputStreamReader(new ReverseLineInputStream(drugref)));
             boolean isMatch1 = false;
@@ -282,37 +328,58 @@ public class Audit {
             if (!flag4)
                 System.out.println("\"drugref_url\" tag not configured properly.");     
         } catch (Exception e) {
-            System.out.println("Drugref verification error: " + e);
+            System.out.println("Could not read properties file to verify Drugref tags: " + e);
         }
     }
 
     /*
-    *  verifyTomcat():
+    *  tomcatReinforcement():
     *  Read "JAVA_OPTS" tag in properties file.
     */
     private static void tomcatReinforcement() {
         try {
-            String s;
-            File tomcat = new File("/etc/default/tomcat7");
-            BufferedReader br = new BufferedReader(new InputStreamReader(new ReverseLineInputStream(tomcat)));
-            boolean isMatch = false;
-
+            String s = "";
+            String xmx = "";
+            String xms = "";
+            Process p = Runtime.getRuntime().exec(new String[]{"sh", "-c", "/bin/ps -ef | /bin/grep tomcat"});
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            boolean isMatch1 = false;
+            boolean isMatch2 = false;
+            boolean flag1 = false;
+            boolean flag2 = false;
+            
+            System.out.println("Currently checking Tomcat reinforcement...");
             while ((s = br.readLine()) != null) {
-                if (Pattern.matches("^(#).*", s))
-                    continue;
-                isMatch = Pattern.matches("^(JAVA_OPTS=).*", s);
-                if (isMatch) {
-                    if (!s.substring(10).equals("")) { // how to check for increased memory resources?
-                        System.out.println("Tomcat is reinforced.");
-                        break;
-                    }
+                Pattern pattern1 = Pattern.compile(".*(Xmx[0-9]+m).*");
+                Matcher matcher1 = pattern1.matcher(s);
+                isMatch1 = matcher1.matches();
+                Pattern pattern2 = Pattern.compile(".*(Xms[0-9]+m).*");
+                Matcher matcher2 = pattern2.matcher(s);
+                isMatch2 = matcher2.matches();
+
+                if (isMatch1) {
+                    xmx = matcher1.group(1);
+                    String[] xmxString = xmx.toString().split("x");
+                    flag1 = true;
+                    System.out.println("Xmx value: " + xmxString[1]);
                 }
+                if (isMatch2) {
+                    xms = matcher2.group(1);
+                    String[] xmsString = xms.toString().split("s");
+                    flag2 = true;
+                    System.out.println("Xms value: " + xmsString[1]);
+                }
+                if (flag1 && flag2)
+                    break;
             }
-            if (!isMatch) {
-                System.out.println("Tomcat is not reinforced. Please check your tomcat file.");
+            if (!flag1) {
+                System.out.println("Could not detect Xmx value.");
+            }
+            if (!flag2) {
+                System.out.println("Could not detect Xms value.");
             }
         } catch (Exception e) {
-            System.out.println("Could not detect Tomcat reinforcement: " + e);
+            System.out.println("Could not find Tomcat process to detect amount of memory allocation: " + e);
         }
     }
 
@@ -333,8 +400,9 @@ public class Audit {
 
         try {
             String s = "";
-            Process p = Runtime.getRuntime().exec(new String[]{"sh", "-c", "ps -ef | grep tomcat7"});
+            Process p = Runtime.getRuntime().exec(new String[]{"sh", "-c", "/bin/ps -ef | /bin/grep tomcat"});
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
             while ((s = br.readLine()) != null) {
                 Pattern pattern = Pattern.compile(regex);
                 Matcher matcher = pattern.matcher(s);
@@ -401,6 +469,7 @@ public class Audit {
             String str;
             Process p = Runtime.getRuntime().exec("which lsb_release");
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
             while ((str = br.readLine()) != null) {
                 return true;
             }
@@ -416,20 +485,20 @@ public class Audit {
     /////////////////////////////////
 
     public static void main(String args[]) {
+        System.out.println("CATEGORY 1 AUDIT: ");
         // Verify operating system && run corresponding functions
-        String os = System.getProperty("os.name");
-
-        if (os.toLowerCase().equals("linux")) {
+        //String os = System.getProperty("os.name");
+        //if (os.toLowerCase().equals("linux")) {
             serverVersion();
             verifyTomcat();
             mysqlVersion();
             verifyOscar();
             verifyDrugref();
             tomcatReinforcement();
-        } else if (os.toLowerCase().equals("windows")) { // fix me to RE
+        //} else if (os.toLowerCase().equals("windows")) { // fix me to RE
 
-        } else { // unix
+        //} else { // unix
 
-        }
+        //}
     }
 }
