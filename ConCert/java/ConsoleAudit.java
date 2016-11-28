22f7aa8993f84a1bf43a8c573016aa2a7860091e
williamgrosset <will_grosset@hotmail.com>
2016-11-28 00:26:25 -0800

    add outdated comment for ConsoleAudit.java
    Change-Id: Id48102c09e983b792e245c7df79764c4a5699c9f

--- ConCert/java/ConsoleAudit.java
+++ ConCert/java/ConsoleAudit.java
@@ -1,3 +1,7 @@
+/***********************************
+ * OUTDATED: PLEASE SEE Audit.java * 
+ ***********************************/
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.File;
