ba156c813682cd5c311917412e9fad22bb574d0b
williamgrosset <will_grosset@hotmail.com>
2017-01-22 23:25:18 -0800

    more tests (Drugref)
    Change-Id: I38cbcd90ae213095e6b87a5d3cc9e56d8ea6299b

--- ConCert/java/test/AuditTest.java
+++ ConCert/java/test/AuditTest.java
@@ -73,23 +73,52 @@ public class AuditTest {
     @Test
     public void isMatchAllVerifyOscarProperties() throws IOException {
         File tempFile = folder.newFile("correctInfo.properties");
-        FileUtils.writeStringToFile(tempFile, "HL7TEXT_LABS=true\nSINGLE_PAGE_CHART=yes\nTMP_DIR=/pathtotmpdir/\ndrugref_url=/pathtodrugref/");
-        String expectedResult = "\"drugref_url\" tag is configured as: /pathtodrugref/<br />\"TMP_DIR\" tag is configured as: /pathtotmpdir/<br />\"SINGLE_PAGE_CHART\" tag is configured as: yes<br />\"HL7TEXT_LABS\" tag is configured as: true<br />";
+        FileUtils.writeStringToFile(tempFile, "HL7TEXT_LABS=true\n"
+                                                + "SINGLE_PAGE_CHART=yes\n"
+                                                + "TMP_DIR=/pathtotmpdir/\n"
+                                                + "drugref_url=/pathtodrugref/");
+        String expectedResult = "\"drugref_url\" tag is configured as: /pathtodrugref/<br />"
+                                    + "\"TMP_DIR\" tag is configured as: /pathtotmpdir/<br />"
+                                    + "\"SINGLE_PAGE_CHART\" tag is configured as: yes<br />"
+                                    + "\"HL7TEXT_LABS\" tag is configured as: true<br />";
         assertEquals(expectedResult, Audit.verifyOscarProperties(tempFile.getPath()));
     }
 
     // isMatch1, !isMatch2, isMatch3, !isMatch4
     @Test
     public void isMatch13VerifyOscarProperties() throws IOException {
-
-
+        File tempFile = folder.newFile("semiCorrectInfo.properties");
+        FileUtils.writeStringToFile(tempFile, "HL7TEXT_LABS=true\n"
+                                                + "#SINGLE_PAGE_CHART=yes\n"
+                                                + "TMP_DIR=/pathtotmpdir/\n"
+                                                + "#drugref_url=/pathtodrugref/");
+        String expectedResult = "\"TMP_DIR\" tag is configured as: /pathtotmpdir/<br />"
+                                    + "\"HL7TEXT_LABS\" tag is configured as: true<br />"
+                                    + "\"SINGLE_PAGE_CHART\" tag is not configured properly.<br />"
+                                    + "\"drugref_url\" tag is not configured properly.<br />";
+        assertEquals(expectedResult, Audit.verifyOscarProperties(tempFile.getPath()));
     }
 
     // !isMatch1, !isMatch2, !isMatch3, !isMatch4
     @Test
     public void isMatchFalseVerifyOscarProperties() throws IOException {
+        File tempFile = folder.newFile("nonCorrectInfo.properties");
+        FileUtils.writeStringToFile(tempFile, "#HL7TEXT_LABS=true\n"
+                                                + "#SINGLE_PAGE_CHART=yes\n"
+                                                + "#TMP_DIR=/pathtotmpdir/\n"
+                                                + "#drugref_url=/pathtodrugref/");
+        String expectedResult = "\"HL7TEXT_LABS\" tag is not configured properly.<br />"
+                                    + "\"SINGLE_PAGE_CHART\" tag is not configured properly.<br />"
+                                    + "\"TMP_DIR\" tag is not configured properly.<br />"
+                                    + "\"drugref_url\" tag is not configured properly.<br />";
+        assertEquals(expectedResult, Audit.verifyOscarProperties(tempFile.getPath()));
+    }
 
-
+    @Test
+    public void exceptionVerifyOscarProperties() throws IOException {
+        File tempFile = folder.newFile("file.txt");
+        String expectedResult = "Could not read properties file to verify Oscar tags.";      
+        assertEquals(expectedResult, Audit.verifyOscarProperties(tempFile.toString()));
     }
 
     /*
@@ -102,21 +131,46 @@ public class AuditTest {
     // isMatch1, isMatch2, isMatch3
     @Test
     public void isMatchAllVerifyDrugrefProperties() throws IOException {
-
-
+        File tempFile = folder.newFile("semiCorrectInfo.properties");
+        FileUtils.writeStringToFile(tempFile, "db_user=root\n"
+                                                + "db_url=jdbc:mysql://127.0.0.1:3306/drugref\n"
+                                                + "db_driver=com.mysql.jdbc.Driver\n");
+        String expectedResult = "\"db_driver\" tag is configured as: com.mysql.jdbc.Driver<br />"
+                                    + "\"db_url\" tag is configured as: jdbc:mysql://127.0.0.1:3306/drugref<br />"
+                                    + "\"db_user\" tag is configured as: root<br />";
+        assertEquals(expectedResult, Audit.verifyDrugrefProperties(tempFile.getPath()));
     }
 
     // isMatch1, isMatch2, !isMatch3
     @Test
     public void isMatch12VerifyDrugrefProperties() throws IOException {
-
-
+        File tempFile = folder.newFile("semiCorrectInfo.properties");
+        FileUtils.writeStringToFile(tempFile, "db_user=root\n"
+                                                + "db_url=jdbc:mysql://127.0.0.1:3306/drugref\n"
+                                                + "#db_driver=com.mysql.jdbc.Driver\n");
+        String expectedResult = "\"db_url\" tag is configured as: jdbc:mysql://127.0.0.1:3306/drugref<br />"
+                                    + "\"db_user\" tag is configured as: root<br />"
+                                    + "\"db_driver\" tag is not configured properly.<br />";
+        assertEquals(expectedResult, Audit.verifyDrugrefProperties(tempFile.getPath()));
     }
 
     // !isMatch1, !isMatch2, !isMatch3
     @Test
     public void isMatchFalseVerifyDrugrefProperties() throws IOException {
+        File tempFile = folder.newFile("semiCorrectInfo.properties");
+        FileUtils.writeStringToFile(tempFile, "#db_user=root\n"
+                                                + "#db_url=jdbc:mysql://127.0.0.1:3306/drugref\n"
+                                                + "#db_driver=com.mysql.jdbc.Driver\n");
+        String expectedResult = "\"db_user\" tag is not configured properly.<br />"
+                                    + "\"db_url\" tag is not configured properly.<br />"
+                                    + "\"db_driver\" tag is not configured properly.<br />";
+        assertEquals(expectedResult, Audit.verifyDrugrefProperties(tempFile.getPath()));
+    }
 
-
+    @Test
+    public void exceptionVerifyDrugrefProperties() throws IOException {
+        File tempFile = folder.newFile("file.txt");
+        String expectedResult = "Could not read properties file to verify Drugref tags.";      
+        assertEquals(expectedResult, Audit.verifyDrugrefProperties(tempFile.toString()));
     }
 }
