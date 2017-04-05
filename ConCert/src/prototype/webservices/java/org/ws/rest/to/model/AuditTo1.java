cce44a05b04046749bb487342b2b5a2091688f18
William Grosset <will_grosset@hotmail.com>
2017-04-05 18:17:29 +0000

    add AuditTo1 class

--- ConCert/src/prototype/webservices/java/org/ws/rest/to/model/AuditTo1.java
+++ ConCert/src/prototype/webservices/java/org/ws/rest/to/model/AuditTo1.java
@@ -1,1 +1,81 @@
-public class AuditTo1 {}
+package org.oscarehr.ws.rest.to.model;
+
+import java.io.Serializable;
+
+public class AuditTo1 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String serverVersion;
+    private String databaseInfo;
+    private String jvmVersion;
+    private String tomcatVersion;
+    private String memoryAllocation;
+    private String oscarBuild;
+    private String oscarProperties;
+    private String drugrefProperties;
+
+    public String getServerVersion() {
+        return serverVersion;
+    }
+
+    public String getDatabaseInfo() {
+        return databaseInfo;
+    }
+
+    public String getJvmVersion() {
+        return jvmVersion;
+    }
+
+    public String getTomcatVersion() {
+        return tomcatVersion;
+    }
+
+    public String getMemoryAllocation() {
+        return memoryAllocation;
+    }
+
+    public String getOscarBuild() {
+        return oscarBuild;
+    }
+
+    public String getOscarProperties() {
+        return oscarProperties;
+    }
+
+    public String getDrugrefProperties() {
+        return drugrefProperties;
+    }
+
+    public void setServerVersion(String serverVersion) {
+        this.serverVersion = serverVersion;
+    }
+
+    public void setDatabaseInfo(String databaseInfo) {
+        this.databaseInfo = databaseInfo;
+    }
+
+    public void setJvmVersion(String jvmVersion) {
+        this.jvmVersion = jvmVersion;
+    }
+
+    public void setTomcatVersion(String tomcatVersion) {
+        this.tomcatVersion = tomcatVersion;
+    }
+
+    public void setMemoryAllocation(String memoryAllocation) {
+        this.memoryAllocation = memoryAllocation;
+    }
+
+    public void setOscarBuild(String oscarBuild) {
+        this.oscarBuild = oscarBuild;
+    }
+
+    public void setOscarProperties(String oscarProperties) {
+        this.oscarProperties = oscarProperties;
+    }
+
+    public void setDrugrefProperties(String drugrefProperties) {
+        this.drugrefProperties = drugrefProperties;
+    }
+}
